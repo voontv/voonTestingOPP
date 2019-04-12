b2f2f5f8079ef86bd9e93d9bbbd8d2f16cac276e
voontv <voontv@gmail.com>
2019-04-12 09:38:40 +0700

    Refactoring OPP shape

--- src/test/java/StrategyShapeTest.java
+++ src/test/java/StrategyShapeTest.java
@@ -1,20 +1,5 @@
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.util.Scanner;
-
 public class StrategyShapeTest {
 
-    @Test
-    public void testcreatedObjectwithParameters() {
-        StrategyShape strategyShape = new StrategyShape();
-        ByteArrayInputStream in = new ByteArrayInputStream("My string".getBytes());
-        System.setIn(in);
-
-// do your thing
-
-// optionally, reset System.in to its original
-        System.setIn(System.in);
-    }
 }
